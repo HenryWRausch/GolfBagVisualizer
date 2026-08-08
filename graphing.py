8404db105c5af06beb361d5f996e82e3536ad68f
Henry Rausch <henrywrausch@gmail.com>
2026-08-08 19:27:08 -0400

    BagGrapher basics

---
+++ graphing.py
@@ -0,0 +1,12 @@
+from database import BagDatabase
+
+
+class BagGrapher:
+    # holds a database connection to access data for a player
+    # contains methods for generating graphs,
+
+    def __init__(self, name: str = "Demo"):
+
+        # create our basics
+        self.db = BagDatabase(name)
+        self.name = name
